a7ff582cd13bf86c483b953e04962c4275ee2a0b
Denys Smirnov <dennwc@pm.me>
2024-12-12 14:49:40 +0200

    Report if fuse was broken by a call to Break.

--- fuse.go
+++ fuse.go
@@ -56,9 +56,13 @@ func (f *Fuse) Watch() <-chan struct{} {
 	return f.c
 }
 
-// Break breaks the fuse
-func (f *Fuse) Break() {
-	f.Once(nil)
+// Break breaks the fuse. It returns true if it was broken by this call.
+func (f *Fuse) Break() bool {
+	broken := false
+	f.Once(func() {
+		broken = true
+	})
+	return broken
 }
 
 // Once runs the callback and breaks the fuse
